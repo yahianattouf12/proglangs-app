064ce7c32fb355eb8828bb041f24c13f8f03d94a
yahia nattouf <yahianattouf12@gmail.com>
2024-11-22 21:15:25 +0200

    init migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,13 +14,14 @@ return new class extends Migration
      */
     public function up(): void
     {
+
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique()->nullable();
-            $table->string('phone_number',  10)->unique();
-            $table->string('password');
+            $table->string('first_name', 50);
+            $table->string('last_name', 50);
+            $table->string('email', 50)->unique()->nullable();
+            $table->string('phone', 10)->unique();
+            $table->string('password', 60);
             $table->string('image', 300)->nullable();
             $table->string('address')->nullable();
             $table->string('role', 6)->default('client');
