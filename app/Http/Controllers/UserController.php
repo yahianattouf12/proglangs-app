91ed455e50d820402714c9c9c031abf33664a46f
ehsan <ehsansawan7@gmail.com>
2024-11-25 14:38:28 -0800

    update function for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -92,4 +92,32 @@ class UserController extends Controller
         $req->user()->currentAccessToken()->delete();
         return response()->json(['message' => 'logout successfully']);
     }
+
+    public function update(Request $req)
+    {
+        $user=$req->user();
+
+         //validation
+        //———————————————————————————————————————————————
+        $valid = Validator::make($req->all(), [
+            'first_name' => 'string|min:2|max:50',
+            'last_name' => 'string|min:2|max:50',
+            'email' => 'email|string|unique:users,email|min:6|max:50',
+            'address' => 'string|min:2|max:50'
+            //we have to add choice to change photo don't forget that
+        ]);
+        if($valid->fails()) return response()->json(['errors' => $valid->errors()]);
+
+        $user->update(
+            [
+                'first_name' => $req->first_name,
+                'last_name' => $req->last_name,
+                'address'=> $req->address,
+                'email' => $req->email,
+            ]
+            );
+
+        return response()->json($user,200);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -6,15 +6,18 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $req) {
-//     return $req->user();
-// })->middleware('auth:sanctum');
+ Route::get('/user', function (Request $req) {
+     return $req->user();
+ })->middleware('auth:sanctum');
 
+//user routes
 Route::post('/user/login', [UserController::class, 'login'])->name('user.login'); 
 
 Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
 
 Route::middleware('auth:sanctum')->post('/user/logout', [UserController::class, 'logout'])->name('user.logout');
+
+Route::post('user/{id}', [UserController::class, 'update'])->name('user.update')->name('user.update')->middleware('auth:sanctum'); 
 
 //Routes for shops
 Route::resource('shops', ShopController::class);
@@ -22,4 +25,4 @@ Route::resource('shops', ShopController::class);
 
 //Routes for products
 Route::resource('products', ProductController::class);
-Route::get('/products/showshopproducts/{id}', [ProductController::class,'showShopProducts']);
+Route::get('/products/showshopproducts/{id}', [ProductController::class,'showShopProducts'])->name('products.showShopProducts');
