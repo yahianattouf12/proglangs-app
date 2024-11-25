2ede63c6b3b8fd8476f04b075846a405377497dc
ehsan <ehsansawan7@gmail.com>
2024-11-25 15:31:45 -0800

    edit on update function for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -99,13 +99,27 @@ class UserController extends Controller
 
          //validation
         //———————————————————————————————————————————————
-        $valid = Validator::make($req->all(), [
-            'first_name' => 'string|min:2|max:50',
-            'last_name' => 'string|min:2|max:50',
-           'email' => 'email|string|unique:users,email|min:6|max:50',
-            'address' => 'string|min:2|max:50'
-            //we have to add choice to change photo don't forget that
-        ]);
+        if($user->email==$req->email)
+        {
+            $valid = Validator::make($req->all(), [
+                'first_name' => 'string|min:2|max:50',
+                'last_name' => 'string|min:2|max:50',
+                'address' => 'string|min:2|max:50'
+                //we have to add choice to change photo don't forget that
+            ]);
+        }
+        else
+        {
+            $valid = Validator::make($req->all(), [
+                'first_name' => 'string|min:2|max:50',
+                'last_name' => 'string|min:2|max:50',
+               'email' => 'email|string|unique:users,email|min:6|max:50',
+                'address' => 'string|min:2|max:50'
+                //we have to add choice to change photo don't forget that
+            ]);
+        }
+
+
         if($valid->fails()) return response()->json(['errors' => $valid->errors()]);
 
         $user->update(
