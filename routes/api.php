64dbb7b64a89d9930fe16ef4e6beb2c640506b37
ehsan <ehsansawan7@gmail.com>
2024-11-25 13:22:14 -0800

    create shop and prodcut controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ShopController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -13,3 +15,11 @@ Route::post('/user/login', [UserController::class, 'login'])->name('user.login')
 Route::post('/user/register', [UserController::class, 'register'])->name('user.register');
 
 Route::middleware('auth:sanctum')->post('/user/logout', [UserController::class, 'logout'])->name('user.logout');
+
+//Routes for shops
+Route::resource('shops', ShopController::class);
+
+
+//Routes for products
+Route::resource('products', ProductController::class);
+Route::get('/products/showshopproducts/{id}', [ProductController::class,'showShopProducts']);
